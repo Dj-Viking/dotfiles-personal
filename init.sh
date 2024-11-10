a63798b037d9b8d8e49b3bfeab3dab8fd9237c45
dj-viking <anders.swedishviking@gmail.com>
2024-11-10 11:17:48 -0500

    fix condition

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 # first clone nvim files from my fork
 
-if [ -z ./.config/nvim ]; then
+if ! [ -d ./.config/nvim ]; then
    git clone https://github.com/Dj-Viking/nvim-files.git ./.config/nvim
 fi
