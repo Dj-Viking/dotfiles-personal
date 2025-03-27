f3fe97bb0b66f15769b4d9b979068a53b07e7bad
dj-viking <anders.swedishviking@gmail.com>
2025-03-27 18:56:27 -0400

    exec_once for the pipewire stuff

--- .bash_functions.sh
+++ .bash_functions.sh
@@ -13,40 +13,43 @@ function mabranch() {
     echo $(git branch 2> /dev/null | grep -e ^* | sed -E s/^\\\*\ \(.+\)$/\\\1\ /)
 }
 
-function kill_pipewire () {
-	# switch off pipewire and wireplumber
-	$(echo pgrep -a pipewire)
-	if [ "$?" = "0" ]; then pkill pipewire; fi 
-	$(echo pgrep -a wireplumber)
-	if [ "$?" = "0" ]; then pkill wireplumber; fi 
-	$(echo pgrep -a pipewire-pulse)
-	if [ "$?" = "0" ]; then pkill pipewire-pulse; fi 
+# function kill_pipewire () {
+# 	# switch off pipewire and wireplumber
+# 	$(echo pgrep -a pipewire)
+# 	if [ "$?" = "0" ]; then pkill pipewire; fi 
+# 	$(echo pgrep -a wireplumber)
+# 	if [ "$?" = "0" ]; then pkill wireplumber; fi 
+# 	$(echo pgrep -a pipewire-pulse)
+# 	if [ "$?" = "0" ]; then pkill pipewire-pulse; fi 
+#
+# 	#switch on pulseaudio
+# 	# pulseaudio --check;
+# 	# if [ "$?" = "1" ]; then pulseaudio --start; fi
+#
+# 	echo "killed pipewire";
+# 	echo "-----------------------------------------------------";
+# }
 
-	#switch on pulseaudio
-	# pulseaudio --check;
-	# if [ "$?" = "1" ]; then pulseaudio --start; fi
+# function start_pipewire () {
+#
+# 	# don't need pulseaudio anymore
+# 	#switch off pulse audio if it's running check the exit code of --check
+# 	# pulseaudio --check;
+# 	# if [ "$?" = "0" ]; then pkill pulseaudio; fi
+#
+# 	# run pipewire and wireplumber
+# 	$(echo pgrep -a pipewire)
+# 	if [ "$?" = "1" ]; then (setsid pipewire &); fi 
+# 	$(echo pgrep -a wireplumber)
+# 	if [ "$?" = "1" ]; then (setsid wireplumber &); fi 
+# 	$(echo pgrep -a pipewire-pulse)
+# 	if [ "$?" = "1" ]; then (setsid pipewire-pulse &); fi 
+#
+# 	echo "pipewire, wireplumber, and pipewire-pulse has started";
+# 	echo "-----------------------------------------------------";
+# 	export PW_STARTED="1"
+# }
 
-	echo "killed pipewire";
-	echo "-----------------------------------------------------";
+function gtkradiant() {
+	(setsid ~/./projects/GtkRadiant/install/radiant.bin &)
 }
-
-function start_pipewire () {
-
-	# don't need pulseaudio anymore
-	#switch off pulse audio if it's running check the exit code of --check
-	# pulseaudio --check;
-	# if [ "$?" = "0" ]; then pkill pulseaudio; fi
-
-	# run pipewire and wireplumber
-	$(echo pgrep -a pipewire)
-	if [ "$?" = "1" ]; then (setsid pipewire &); fi 
-	$(echo pgrep -a wireplumber)
-	if [ "$?" = "1" ]; then (setsid wireplumber &); fi 
-	$(echo pgrep -a pipewire-pulse)
-	if [ "$?" = "1" ]; then (setsid pipewire-pulse &); fi 
-
-	echo "pipewire, wireplumber, and pipewire-pulse has started";
-	echo "-----------------------------------------------------";
-	export PW_STARTED="1"
-}
-
